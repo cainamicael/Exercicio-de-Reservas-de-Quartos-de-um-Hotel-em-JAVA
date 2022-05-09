14189d959c1b74a083350c1b5d0f4cc0ae5c3fb1
cainamicael <cainamicaeloficial@gmail.com>
2022-05-09 18:22:51 -0300

    Update Quartos.java

--- Codificação/Reservas da Pousada/src/classes/Quartos.java
+++ Codificação/Reservas da Pousada/src/classes/Quartos.java
@@ -4,4 +4,25 @@ public class Quartos {
     public String nome;
     public String email;
     public int quarto;
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public int getQuarto() {
+        return quarto;
+    }
+    public void setQuarto(int quarto) {
+        this.quarto = quarto;
+    }
+
+    
+
 }
